fb05bdb46870ed3b9403a1b34388d65b4b2b274e
Javier Lete <javier@eskura21.com>
2022-04-21 10:49:21 +0200

    Ampliación de DOM y AJAX

--- MF0491/UF1842/js/ajax.js
+++ MF0491/UF1842/js/ajax.js
@@ -1,16 +1,56 @@
 'use strict';
 
+// window.addEventListener('DOMContentLoaded', function () {
+//     const lista = document.getElementById('lista');
+
+//     fetch('json/datos.json').then(
+//         function (respuesta) { return respuesta.json(); }
+//     ).then(function (datos) {
+//         var li;
+//         for (var dato of datos) {
+//             li = document.createElement('li');
+//             li.innerText = dato.id + ': ' + dato.nombre;
+
+//             lista.appendChild(li);
+//         }
+//     }
+//     );
+// });
+
+// window.addEventListener('DOMContentLoaded', function () {
+//     const lista = document.getElementById('lista');
+
+//     fetch('json/datos.json').then(
+//         respuesta => respuesta.json()
+//     ).then(datos => {
+//         let li;
+//         for (let dato of datos) {
+//             li = document.createElement('li');
+//             li.innerText = dato.id + ': ' + dato.nombre;
+
+//             lista.appendChild(li);
+//         }
+//     }
+//     );
+// });
+
+
 window.addEventListener('DOMContentLoaded', async function () {
     const lista = document.getElementById('lista');
 
-    const respuesta = await fetch('json/datos.json');
-    const datos = await respuesta.json();
+    try {
+        const respuesta = await fetch('json/datos.json');
+        const datos = await respuesta.json();
 
-    let li;
-    for(let dato of datos) {
-        li = document.createElement('li');
-        li.innerText = dato.id + ': ' + dato.nombre;
+        let li;
+        for (let dato of datos) {
+            li = document.createElement('li');
+            li.innerText = dato.id + ': ' + dato.nombre;
 
-        lista.appendChild(li);
+            lista.appendChild(li);
+        }
+    } catch (error) {
+        alert('No se ha podido conectar a los datos');
+        console.error(error);
     }
 });
--- MF0491/UF1842/js/dom.js
+++ MF0491/UF1842/js/dom.js
@@ -3,6 +3,8 @@
 // window.onload = function() { ... }
 
 window.addEventListener('DOMContentLoaded', function () {
+    console.log('DENTRO');
+
     const h1s = document.getElementsByTagName('h1');
 
     for (let h1 of h1s) {
@@ -20,12 +22,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
         console.log(boton.parentNode);
 
-        respuesta.innerText = 'Hola, ' + nombre.value;
+        respuesta.innerText = 'Hola ' + nombre.value;
         
         // boton.parentNode.appendChild(respuesta);
 
-        boton.insertAdjacentElement('afterEnd', respuesta);
+        boton.insertAdjacentElement('afterend', respuesta);
 
         document.forms[0].innerHTML += '<span>blablabla</span>';
     });
 });
+
+console.log('FUERA');
