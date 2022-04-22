ec80a8dc44866757a07fa01786e3f217cfb75212
Javier Lete <javier@eskura21.com>
2022-04-22 08:33:03 +0200

    Calculadora1

---
+++ MF0491/UF1842/js/calculadora1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const op1 = document.getElementById('op1');
+    const op = document.getElementById('op');
+    const op2 = document.getElementById('op2');
+    const btnIgual = document.getElementById('btn-igual');
+    const res = document.getElementById('res');
+
+    btnIgual.addEventListener('click', function() {
+        const a = +op1.value;
+        const b = +op2.value;
+        const operador = op.value;
+        let resultado;
+
+        switch(operador) {
+            case '+': resultado = a + b; break;
+            case '-': resultado = a - b; break;
+            case '*': resultado = a * b; break;
+            case '/': resultado = a / b; break;
+        }
+
+        res.innerText = resultado;
+    });
+});
