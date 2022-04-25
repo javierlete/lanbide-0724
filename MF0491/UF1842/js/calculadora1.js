19dbf66d16abb995eeffc7bc8cfff1cac6482e94
Javier Lete <javier@eskura21.com>
2022-04-25 11:31:57 +0200

    (Formateo)

--- MF0491/UF1842/js/calculadora1.js
+++ MF0491/UF1842/js/calculadora1.js
@@ -1,28 +1,28 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     const op1 = document.getElementById('op1');
     const op = document.getElementById('op');
     const op2 = document.getElementById('op2');
     const btnIgual = document.getElementById('btn-igual');
     const res = document.getElementById('res');
 
-    btnIgual.addEventListener('click', function() {
+    btnIgual.addEventListener('click', function () {
         const a = +op1.value;
         const b = +op2.value;
         const operador = op.value;
         let resultado;
 
-        switch(operador) {
+        switch (operador) {
             case '+': resultado = a + b; break;
             case '-': resultado = a - b; break;
             case '*': resultado = a * b; break;
             case '/': resultado = a / b; break;
         }
 
-       if(isNaN(resultado)){
+        if (isNaN(resultado)) {
             alert("Las entradas tienen que tener ambos un NUMERO!");
-        }else{
+        } else {
             res.innerText = resultado;
         }
     });
