2bb516fe6c9a975279922f7593e6ce02d4f7f086
Javier Lete <javier@eskura21.com>
2022-04-27 08:45:54 +0200

    Incorporación de DataTables.net al cargar la tabla

--- MF0491/UF1842/js/crud.js
+++ MF0491/UF1842/js/crud.js
@@ -62,6 +62,15 @@ async function cargarTabla() {
 
         tbody.appendChild(tr);
     });
+
+    // DATATABLES.NET
+    // $(function () {
+        $('table').DataTable( {
+            language: {
+                url: 'http://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json'
+            }
+        });
+    // });
 }
 
 async function borrar(id) {
