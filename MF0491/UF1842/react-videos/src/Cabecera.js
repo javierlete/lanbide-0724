6550febc2ec002b89f93e56947c127f1135878f6
Javier Lete <javier@eskura21.com>
2022-05-10 10:25:06 +0200

    class -> className

--- MF0491/UF1842/react-videos/src/Cabecera.js
+++ MF0491/UF1842/react-videos/src/Cabecera.js
@@ -3,27 +3,27 @@ import 'bootstrap';
 
 export default function Cabecera() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">VideosApp</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">VideosApp</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Principal</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Principal</a>
                         </li>
                     </ul>
-                    <span class="navbar-text">Usuario</span>
-                    <ul class="navbar-nav mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="admin/videos.html">Administración</a>
+                    <span className="navbar-text">Usuario</span>
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="admin/videos.html">Administración</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Logout</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Logout</a>
                         </li>
                     </ul>
                 </div>
