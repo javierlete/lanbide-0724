f58590af34c79ba7d32565082f7b82b6b5e3d7d2
Javier Lete <javier@eskura21.com>
2022-05-10 10:25:18 +0200

    Pie con modificación de h-100

--- MF0491/UF1842/react-videos/src/Pie.js
+++ MF0491/UF1842/react-videos/src/Pie.js
@@ -1,10 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import { useEffect } from 'react';
 
 export default function Pie() {
-    // TODO: Intentar usar el ciclo de vida para modificar las clases superiores
+    useEffect(() => {
+        const pie = document.getElementById('pie');
+
+        pie.parentElement.classList.add('d-flex');
+        pie.parentElement.classList.add('flex-column');
+
+        let nodo = pie;
+
+        while (nodo.parentElement) {
+            nodo = nodo.parentElement;
+            nodo.classList.add('h-100');
+        }
+    });
+    
     return (
-        <footer className="bg-dark text-light p-3 mt-auto">
+        <footer id="pie" className="bg-dark text-light p-3 mt-auto">
             <p className="text-center">&copy; 2022 - VideosApp</p>
         </footer>);
 }
