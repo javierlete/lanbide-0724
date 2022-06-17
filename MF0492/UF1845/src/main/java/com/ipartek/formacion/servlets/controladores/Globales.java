933e104874dbd8f0a077d536d2b23436017d99eb
javierlete <javierlete@localhost>
2022-06-17 10:33:22 +0200

    Fichero de configuración para DAL

--- MF0492/UF1845/src/main/java/com/ipartek/formacion/servlets/controladores/Globales.java
+++ MF0492/UF1845/src/main/java/com/ipartek/formacion/servlets/controladores/Globales.java
@@ -1,8 +1,26 @@
 package com.ipartek.formacion.servlets.controladores;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import com.ipartek.formacion.servlets.dal.DalException;
 import com.ipartek.formacion.servlets.dal.DaoFabrica;
 import com.ipartek.formacion.servlets.dal.DaoUsuario;
 
 public class Globales {
-	static final DaoUsuario DAO = new DaoFabrica("memoria").getDaoUsuario();
+	static final DaoUsuario DAO;
+	private static final String CONFIGURACION = "configuracion.properties"; 
+	
+	static {
+		try {
+			Properties props = new Properties();
+			props.load(Globales.class.getClassLoader().getResourceAsStream(CONFIGURACION));
+			
+			String tipo = props.getProperty("dal.tipodao");
+			
+			DAO = new DaoFabrica(tipo).getDaoUsuario();
+		} catch (IOException e) {
+			throw new DalException("No se ha podido obtener la configuración");
+		}
+	}
 }
