17ddb7bc01112aff9040dc8c76b5f4b89f807ed7
javierlete <javierlete@localhost>
2022-06-17 10:33:47 +0200

    Filtro de acceso a admin para los logueados

---
+++ MF0492/UF1845/src/main/java/com/ipartek/formacion/servlets/filtros/LoginFilter.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.servlets.filtros;
+
+import java.io.IOException;
+
+import com.ipartek.formacion.servlets.modelos.Usuario;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter("/admin/*")
+public class LoginFilter extends HttpFilter {
+
+	private static final long serialVersionUID = 4199744105791952282L;
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpSession session = httpRequest.getSession();
+
+		// session.setMaxInactiveInterval(0); // Tiempo en segundos de inactividad para cerrar sesión 
+		
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+
+		if (usuario == null) {
+			httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+			return;
+		}
+
+		chain.doFilter(request, response);
+	}
+
+}
