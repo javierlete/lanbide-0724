210729542b07a20776ac947160ac818158faf51d
javierlete <javierlete@localhost>
2022-07-18 09:14:40 +0200

    Configuración XML

--- MF0493/HolaMundoEmpresarial/src/com/ipartek/formacion/holamundo/Fabrica.java
+++
@@ -1,6 +0,0 @@
-package com.ipartek.formacion.holamundo;
-
-public interface Fabrica {
-	Proveedor getProveedor();
-	Salida getSalida();
-}
--- MF0493/HolaMundoEmpresarial/src/com/ipartek/formacion/holamundo/HolaMundo.java
+++ MF0493/HolaMundoEmpresarial/src/com/ipartek/formacion/holamundo/HolaMundo.java
@@ -1,12 +1,16 @@
 package com.ipartek.formacion.holamundo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		Fabrica f = new FabricaImpl();
-		
-		Salida s = f.getSalida();
-		Proveedor p = f.getProveedor();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+		// retrieve configured instance
+		Salida s = context.getBean("salida", Salida.class);
+		Proveedor p = context.getBean("proveedor", Proveedor.class);
 		
 		s.mostrar(p.getMensaje());
 	}
