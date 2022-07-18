a93f50012e7819a32919582dbda820bebf840425
javierlete <javierlete@localhost>
2022-07-18 11:52:07 +0200

    springmvc inicio Spring Starter Project Web, DevTools, Thymeleaf

---
+++ MF0493/springmvc/src/test/java/com/ipartek/formacion/spring/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
