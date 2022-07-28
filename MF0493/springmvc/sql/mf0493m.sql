739e466bfd76691e4400e45f2b500254e36c0a56
javierlete <javierlete@localhost>
2022-07-28 12:02:01 +0200

    Base de datos actualizada con todo

--- MF0493/springmvc/sql/mf0493m.sql
+++ MF0493/springmvc/sql/mf0493m.sql
@@ -43,6 +43,85 @@ INSERT INTO `authorities` VALUES ('javier','ROLE_ADMIN'),('javier','ROLE_USUARIO
 UNLOCK TABLES;
 
 --
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nif` char(9) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'12345678Z'),(2,'87654321A');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facturas`
+--
+
+DROP TABLE IF EXISTS `facturas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facturas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `numero` varchar(255) DEFAULT NULL,
+  `cliente_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK1qiuk10rfkovhlfpsk7oic0v8` (`cliente_id`),
+  CONSTRAINT `FK1qiuk10rfkovhlfpsk7oic0v8` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facturas`
+--
+
+LOCK TABLES `facturas` WRITE;
+/*!40000 ALTER TABLE `facturas` DISABLE KEYS */;
+INSERT INTO `facturas` VALUES (1,'2022-07-28','1',1),(2,'2022-07-28','2',1),(3,'2022-07-28','3',2);
+/*!40000 ALTER TABLE `facturas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facturas_productos`
+--
+
+DROP TABLE IF EXISTS `facturas_productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facturas_productos` (
+  `facturas_id` bigint NOT NULL,
+  `productos_id` bigint NOT NULL,
+  PRIMARY KEY (`facturas_id`,`productos_id`),
+  KEY `FKnkcvy58jujefpkqiiccf0xam7` (`productos_id`),
+  CONSTRAINT `FK86g2j7d98x3ldq51ejxnok6h9` FOREIGN KEY (`facturas_id`) REFERENCES `facturas` (`id`),
+  CONSTRAINT `FKnkcvy58jujefpkqiiccf0xam7` FOREIGN KEY (`productos_id`) REFERENCES `productos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facturas_productos`
+--
+
+LOCK TABLES `facturas_productos` WRITE;
+/*!40000 ALTER TABLE `facturas_productos` DISABLE KEYS */;
+INSERT INTO `facturas_productos` VALUES (1,2),(1,5),(1,7);
+/*!40000 ALTER TABLE `facturas_productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `productos`
 --
 
@@ -67,6 +146,31 @@ LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
 INSERT INTO `productos` VALUES (2,'Ratón',12.00,'2000-01-02',20),(3,'Modifiado',1234.00,'2022-07-27',12),(5,'Nuevoasdfasdfasdf',43424.00,'2022-07-27',12),(7,'Novísimo',4321.00,'2345-12-12',5);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `descripcion` longtext,
+  `nombre` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,NULL,'ADMIN'),(2,NULL,'USUARIOS');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -104,12 +208,17 @@ DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
-  `nick` varchar(255) NOT NULL,
+  `nick` varchar(100) NOT NULL,
   `nombre` varchar(255) DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `rol` varchar(255) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `rol_id` bigint NOT NULL,
+  `cliente_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_msc0amciyjnaxc7adtucv478g` (`nick`)
+  UNIQUE KEY `UK_msc0amciyjnaxc7adtucv478g` (`nick`),
+  KEY `FKqf5elo4jcq7qrt83oi0qmenjo` (`rol_id`),
+  KEY `FKdx76w4skuwj8wldmr7ebfyegq` (`cliente_id`),
+  CONSTRAINT `FKdx76w4skuwj8wldmr7ebfyegq` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`),
+  CONSTRAINT `FKqf5elo4jcq7qrt83oi0qmenjo` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -119,7 +228,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'juan@gonzalez.net','juan','Juan','$2a$10$WRPMcW0P/TrSZe5T9bDNTuAXPKuOCbtURu/o4zc7KQr.kRp8KinCe','USUARIO'),(2,'javier@email.net','javier','Javier','$2a$10$IyitE0Vw.8/R913BiMdNjume38eNoMe1y6AGjp3LffBjHR31JznbK','ADMIN');
+INSERT INTO `usuarios` VALUES (1,'juan@gonzalez.net','juan','Juan','$2a$10$WRPMcW0P/TrSZe5T9bDNTuAXPKuOCbtURu/o4zc7KQr.kRp8KinCe',2,1),(2,'javier@email.net','javier','Javier','$2a$10$IyitE0Vw.8/R913BiMdNjume38eNoMe1y6AGjp3LffBjHR31JznbK',1,2);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,4 +249,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-26 10:20:41
+-- Dump completed on 2022-07-28 12:01:15
