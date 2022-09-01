b6c979fc2b79171f533e56481f60be729c91bd98
javierlete <javierlete@localhost>
2022-09-01 09:26:48 +0200

    RestaurantApp

---
+++ MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/RestaurantappApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.restaurantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantappApplication.class, args);
+	}
+
+}
---
+++ MF0493/restaurantapp/src/test/java/com/ipartek/formacion/spring/restaurantapp/RestaurantappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.restaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
