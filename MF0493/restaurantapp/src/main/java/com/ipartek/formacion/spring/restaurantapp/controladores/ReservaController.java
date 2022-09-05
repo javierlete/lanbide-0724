9c4e097cd2ab1a9b243ad024c0fa2d8bb30b6332
javierlete <javierlete@localhost>
2022-09-05 10:41:31 +0200

    Formulario de reserva

--- MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/controladores/ReservaController.java
+++ MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/controladores/ReservaController.java
@@ -1,11 +1,16 @@
 package com.ipartek.formacion.spring.restaurantapp.controladores;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ipartek.formacion.spring.restaurantapp.entidades.Reserva;
 import com.ipartek.formacion.spring.restaurantapp.servicios.ReservaService;
 
 // Usuario por defecto: user
@@ -27,5 +32,25 @@ public class ReservaController {
 		modelo.addAttribute("reservas", servicio.listar());
 		
 		return "listado";
+	}
+	
+	@GetMapping("/crear")
+	public String crear(Reserva reserva) {
+		// reserva.setNombre("Prueba");
+		return "reserva";
+	}
+	
+	@PostMapping("/crear")
+	// @Valid hace que se valide el objeto reserva por medio de todos los atributos
+	// de validación de la entidad (@NotBlank...)
+	// BindingResult nos da el resultado de la validación
+	public String crear(@Valid Reserva reserva, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "reserva";
+		}
+		
+		servicio.crear(reserva);
+		
+		return "redirect:/reserva/listar";
 	}
 }
