379a0cc995bc0ecc81277ccfcbc7ccfcdbc094c1
javierlete <javierlete@localhost>
2022-09-07 10:56:42 +0200

    Uso de RestRepositories para hacer repositorios automáticos por REST

--- MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/repositorios/ReservaRepository.java
+++ MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/repositorios/ReservaRepository.java
@@ -1,11 +1,14 @@
 package com.ipartek.formacion.spring.restaurantapp.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import java.time.LocalDateTime;
 import java.util.Set;
 
 import com.ipartek.formacion.spring.restaurantapp.entidades.Reserva;
 
+@RepositoryRestResource(path = "reservas", collectionResourceRel = "reservas")
 public interface ReservaRepository extends CrudRepository<Reserva, Long> {
 	// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repository-query-keywords
 	Set<Reserva> findByNombreContainsIgnoreCaseOrderByFechaHora(String nombre);
