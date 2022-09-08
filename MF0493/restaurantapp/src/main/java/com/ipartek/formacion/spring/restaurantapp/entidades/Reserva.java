e18423e8105bef413a21d367c0ea8fc6b0a86402
javierlete <javierlete@localhost>
2022-09-08 10:39:24 +0200

    (Quitar imports no necesarios)

--- MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/entidades/Reserva.java
+++ MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/entidades/Reserva.java
@@ -17,8 +17,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
