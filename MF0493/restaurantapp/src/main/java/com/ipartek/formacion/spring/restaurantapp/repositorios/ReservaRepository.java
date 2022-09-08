9bd25a9f5e5246537fb26011725cf239b61de626
javierlete <javierlete@localhost>
2022-09-08 10:17:36 +0200

    Añadimos búsqueda por teléfono y fechaHora

--- MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/repositorios/ReservaRepository.java
+++ MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/repositorios/ReservaRepository.java
@@ -1,10 +1,12 @@
 package com.ipartek.formacion.spring.restaurantapp.repositorios;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import java.time.LocalDateTime;
 import java.util.Set;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import com.ipartek.formacion.spring.restaurantapp.entidades.Reserva;
 
@@ -13,4 +15,6 @@ public interface ReservaRepository extends CrudRepository<Reserva, Long> {
 	// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repository-query-keywords
 	Set<Reserva> findByNombreContainsIgnoreCaseOrderByFechaHora(String nombre);
 	Set<Reserva> findByFechaHoraBetweenOrderByFechaHora(LocalDateTime inicio, LocalDateTime fin);
+	
+	Reserva findByTelefonoAndFechaHora(String telefono, @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime fechaHora);
 }
