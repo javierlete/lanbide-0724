79d28cb3c86d07bf1d6b8b75ea355d671c6306e7
javierlete <javierlete@localhost>
2022-09-09 13:07:06 +0200

    Exponer los IDs en los servicios REST automáticos

---
+++ MF0493/restaurantapp/src/main/java/com/ipartek/formacion/spring/restaurantapp/config/RepositoryRestConfig.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.spring.restaurantapp.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RepositoryRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
+    }
+}
