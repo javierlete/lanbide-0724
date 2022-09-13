d42a29b50d6d4ca7af02382d56273d3245c35775
javierlete <javierlete@localhost>
2022-09-13 11:22:07 +0200

    RepositoryRestResource con documentación automática

--- MF0493/mf0493/src/main/java/com/ipartek/formacion/spring/mf0493/repositorios/InmuebleRepository.java
+++ MF0493/mf0493/src/main/java/com/ipartek/formacion/spring/mf0493/repositorios/InmuebleRepository.java
@@ -4,12 +4,18 @@ import java.math.BigDecimal;
 import java.util.Set;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.ipartek.formacion.spring.mf0493.entidades.Inmueble;
 import com.ipartek.formacion.spring.mf0493.entidades.Inmueble.Tipo;
 
+@RepositoryRestResource(collectionResourceRel = "inmuebles", path = "inmuebles")
 public interface InmuebleRepository extends PagingAndSortingRepository<Inmueble, Long> {
 	Set<Inmueble> findByPrecioBetween(BigDecimal min, BigDecimal max);
 	Set<Inmueble> findByTipo(Tipo tipo);
 	Set<Inmueble> findByDescripcionContainingOrDireccionContaining(String nombre, String direccion);
+	
+	@RestResource(exported = false)
+	void deleteById(Long id);
 }
